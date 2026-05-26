2ea4e4f40edd51dfe4e6e87c12bba38cf49de038
adnan yusuf <adnanyusufhartawan06@gmail.com>
2026-05-26 07:02:21 +0700

    Delete resources directory

--- resources/script.js
+++
@@ -1,6 +0,0 @@
-const map =  L.map('map').setView([-6.8898, 109.6745], 13);
-
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
